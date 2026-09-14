55968e02a3b215a69b30f949997f6580f0baf003
Lee Devin Gerrard <leedevgerrard@gmail.com>
2026-09-14 13:38:47 +0700

    Add unit test for project

--- main/tests.py
+++ main/tests.py
@@ -2,15 +2,21 @@ from django.test import TestCase
 from django.urls import reverse
 from django.utils import timezone
 
-from main.models import Experience
+from main.models import Experience, Project
 
 
 class MainTest(TestCase):
     def setUp(self):
         self.experience = Experience.objects.create(
-            title="Asisten Dosen PBP",
-            description="Membantu mahasiswa memahami pengembangan web.",
+            title="Vice President of Student Council",
+            description="Led and organized the student council with 23 members",
             category="part-time",
+        )
+        self.project = Project.objects.create(
+            title="POSIFIED",
+            description="A web-based Point of Sale application created using MERN stack. Built specifically for culinary Micro, Small, and Medium Enterprises (MSMEs)",
+            thumbnail="../static/img/posified-white-logo.png",
+            thumbnail_alt="Logo of POSIFIED",
         )
 
     def test_main_url_is_accessible(self):
@@ -19,7 +25,9 @@ class MainTest(TestCase):
         self.assertEqual(response.status_code, 200)
         self.assertTemplateUsed(response, "index.html")
         self.assertNotContains(response, self.experience.title)
+        self.assertNotContains(response, self.project.title)
         self.assertContains(response, f'href="{reverse("main:show_experience")}"')
+        self.assertContains(response, f'href="{reverse("main:show_project")}"')
 
     def test_nonexistent_page_returns_404(self):
         response = self.client.get("/halaman-yang-tidak-ada/")
@@ -27,7 +35,7 @@ class MainTest(TestCase):
         self.assertEqual(response.status_code, 404)
 
     def test_experience_model(self):
-        self.assertEqual(str(self.experience), "Asisten Dosen PBP")
+        self.assertEqual(str(self.experience), "Vice President of Student Council")
         self.assertEqual(self.experience.category, "part-time")
         self.assertTrue(self.experience.is_ongoing)
 
@@ -39,14 +47,14 @@ class MainTest(TestCase):
         self.assertContains(response, self.experience.title)
         self.assertContains(response, self.experience.description)
         self.assertContains(response, "Part-Time")
-        self.assertContains(response, "Sedang berlangsung")
         self.assertContains(response, f'href="{reverse("main:show_main")}"')
+        self.assertContains(response, f'href="{reverse("main:show_project")}"')
 
     def test_empty_experience_page(self):
         Experience.objects.all().delete()
         response = self.client.get(reverse("main:show_experience"))
 
-        self.assertContains(response, "Belum ada pengalaman yang ditambahkan.")
+        self.assertContains(response, "No experience added yet.")
 
     def test_completed_experience(self):
         self.experience.ended_at = timezone.now()
@@ -54,5 +62,25 @@ class MainTest(TestCase):
         response = self.client.get(reverse("main:show_experience"))
 
         self.assertFalse(self.experience.is_ongoing)
-        self.assertContains(response, "Selesai")
-        self.assertNotContains(response, "Sedang berlangsung")
+
+    def test_project_model(self):
+        self.assertEqual(str(self.project), "POSIFIED")
+        self.assertEqual(self.project.thumbnail, "../static/img/posified-white-logo.png")
+        self.assertEqual(self.project.thumbnail_alt, "Logo of POSIFIED")
+
+    def test_project_page(self):
+        response = self.client.get(reverse("main:show_project"))
+
+        self.assertEqual(response.status_code, 200)
+        self.assertTemplateUsed(response, "project.html")
+        self.assertContains(response, self.project.title)
+        self.assertContains(response, self.project.description)
+        self.assertContains(response, "POSIFIED")
+        self.assertContains(response, f'href="{reverse("main:show_main")}"')
+        self.assertContains(response, f'href="{reverse("main:show_experience")}"')
+
+    def test_empty_project_page(self):
+        Project.objects.all().delete()
+        response = self.client.get(reverse("main:show_project"))
+
+        self.assertContains(response, "No projects added yet.")
